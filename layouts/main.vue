13d4a132aaa624cabc562df47a40466016053747
martoast <alexmartos96@gmail.com>
2024-06-14 16:56:37 -0700

    hide button rn

--- layouts/main.vue
+++ layouts/main.vue
@@ -46,7 +46,7 @@
               >Get on my investors list</NuxtLink>
             </div>
           </div>
-          <div v-if="!isActiveRoute('/realty-radar')" class="flex items-center">
+          <!-- <div v-if="!isActiveRoute('/realty-radar')" class="flex items-center">
             <div class="flex-shrink-0">
               <button @click="handleNavigation" type="button" class="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 <PlusIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
@@ -54,7 +54,7 @@
               </button>
             </div>
             
-          </div>
+          </div> -->
         </div>
       </div>
 
