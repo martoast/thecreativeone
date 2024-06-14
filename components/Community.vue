1361db53226b40969abce4f9863dcd9548d11018
martoast <alexmartos96@gmail.com>
2024-06-14 00:58:49 -0700

    change community section images

--- components/Community.vue
+++ components/Community.vue
@@ -11,18 +11,7 @@
               <div class="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
                 <img src="https://green-junior-chickadee-977.mypinata.cloud/ipfs/QmSabPGPR2dFEZBAw4sLAfKVUQJcCxAugjctWqECPkZcKo" alt="" class="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover" />
               </div>
-              <div class="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
-                
-                <div class="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
-                  <img src="/public/social2.jpeg" alt="" class="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover" />
-                </div>
-                <div class="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
-                  <img src="/public/social1.jpeg" alt="" class="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover" />
-                </div>
-                <div class="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
-                  <img src="/public/social2.jpeg" alt="" class="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover" />
-                </div>
-              </div>
+              
             </div>
           </div>
         </div>
