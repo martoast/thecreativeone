d624821e27de30c714439e0c13ebfa8b167cf141
martoast <alexmartos96@gmail.com>
2024-06-23 18:17:21 -0700

    new image

--- components/CallAction.vue
+++ components/CallAction.vue
@@ -3,7 +3,7 @@
       <div class="relative isolate">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div class="relative overflow-hidden bg-gray-900 px-6 py-20 shadow-xl sm:rounded-3xl sm:px-10 sm:py-24 md:px-12 lg:px-20">
-            <img class="absolute inset-0 h-full w-full object-cover object-top brightness-150 saturate-0" src="https://images.unsplash.com/photo-1519338381761-c7523edc1f46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="" />
+            <img class="absolute inset-0 h-full w-full object-cover object-top brightness-150 saturate-0" src="/skyline.jpg" alt="" />
             <div class="absolute inset-0 bg-gray-900/90 mix-blend-multiply" />
             <div class="absolute -left-80 -top-56 transform-gpu blur-3xl" aria-hidden="true">
               <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-[0.45]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
