3b6f4da00e803c649bf6bf59a00954e899dd6ab7
martoast <alexmartos96@gmail.com>
2024-06-30 09:32:59 -0700

    fix ssr

--- plugins/mapboxsearch.js
+++ plugins/mapboxsearch.js
@@ -1,5 +1,10 @@
-import * as mapboxsearch from '@mapbox/search-js-web';
-
-export default defineNuxtPlugin(nuxtApp => {
-    nuxtApp.mapboxsearch = mapboxsearch
-  })
+export default defineNuxtPlugin((nuxtApp) => {
+  // Only run on client-side
+  if (process.client) {
+    // Use dynamic import to load the module only on the client side
+    return import('@mapbox/search-js-web').then((mapboxsearch) => {
+      // Make mapboxsearch available in the Nuxt app
+      nuxtApp.provide('mapboxsearch', mapboxsearch)
+    })
+  }
+})
