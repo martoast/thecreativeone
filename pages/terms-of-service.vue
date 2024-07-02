46315f8c7eff22c86db39d9b878c54284b7730e4
martoast <alexmartos96@gmail.com>
2024-07-02 13:35:37 -0700

    terms of service

---
+++ pages/terms-of-service.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="bg-white px-6 py-32 lg:px-8">
+  <div class="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+    <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Terms of Service</h1>
+    <p class="mt-6 text-xl leading-8">Last Updated: 07/02/2024</p>
+    
+    <p class="mt-6">Welcome to thecreativeone. These Terms of Service ("Terms") govern your access to and use of thecreativeone's website, services, and applications (the "Service"). Please read these Terms carefully, and contact us if you have any questions. By accessing or using our Service, you agree to be bound by these Terms and our Privacy Policy.</p>
+
+    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">1. Definitions</h2>
+    <ul role="list" class="mt-8 max-w-xl space-y-8 text-gray-600">
+      <li class="flex gap-x-3">
+        <span><strong class="font-semibold text-gray-900">"Company"</strong> refers to thecreativeone, 1401 21st Street Suite R
+Sacramento California 95811.</span>
+      </li>
+      <li class="flex gap-x-3">
+        <span><strong class="font-semibold text-gray-900">"Service"</strong> refers to the website, applications, and services provided by thecreativeone.</span>
+      </li>
+      <li class="flex gap-x-3">
+        <span><strong class="font-semibold text-gray-900">"You"</strong> refers to the individual accessing or using the Service, or the company or organization on whose behalf that individual is accessing or using the Service.</span>
+      </li>
+    </ul>
+
+    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">2. Use of Service</h2>
+    <p class="mt-6">You may use our Service only if you can form a binding contract with thecreativeone, and only in compliance with these Terms and all applicable laws. When you create your thecreativeone account, you must provide us with accurate and complete information. Any use or access by anyone under the age of 18 is prohibited.</p>
+
+    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">3. Account Registration</h2>
+    <p class="mt-6">To access certain features of the Service, you may be required to register for an account. You agree to provide accurate, current, and complete information during the registration process and to update such information to keep it accurate, current, and complete.</p>
+
+    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">4. Content and Conduct</h2>
+    <p class="mt-6">Our Service allows you to post, link, store, share and otherwise make available certain information, text, graphics, or other material ("Content"). You are responsible for the Content that you post on or through the Service, including its legality, reliability, and appropriateness.</p>
+
+    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">5. Intellectual Property Rights</h2>
+    <p class="mt-6">The Service and its original content, features, and functionality are and will remain the exclusive property of thecreativeone and its licensors. The Service is protected by copyright, trademark, and other laws of both the United States and foreign countries. Our trademarks and trade dress may not be used in connection with any product or service without the prior written consent of thecreativeone.</p>
+
+    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">6. Links to Other Websites</h2>
+    <p class="mt-6">Our Service may contain links to third-party websites or services that are not owned or controlled by thecreativeone. thecreativeone has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third-party websites or services.</p>
+
+    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">7. Termination</h2>
+    <p class="mt-6">We may terminate or suspend your account and bar access to the Service immediately, without prior notice or liability, under our sole discretion, for any reason whatsoever and without limitation, including but not limited to a breach of the Terms.</p>
+
+    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">8. Limitation of Liability</h2>
+    <p class="mt-6">In no event shall thecreativeone, nor its directors, employees, partners, agents, suppliers, or affiliates, be liable for any indirect, incidental, special, consequential or punitive damages, including without limitation, loss of profits, data, use, goodwill, or other intangible losses, resulting from your access to or use of or inability to access or use the Service.</p>
+
+    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">9. Disclaimer</h2>
+    <p class="mt-6">Your use of the Service is at your sole risk. The Service is provided on an "AS IS" and "AS AVAILABLE" basis. The Service is provided without warranties of any kind, whether express or implied, including, but not limited to, implied warranties of merchantability, fitness for a particular purpose, non-infringement or course of performance.</p>
+
+    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">10. Governing Law</h2>
+    <p class="mt-6">These Terms shall be governed and construed in accordance with the laws of Texas, United States of America, without regard to its conflict of law provisions.</p>
+
+    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">11. Changes to Terms</h2>
+    <p class="mt-6">We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will provide at least 30 days' notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</p>
+
+    <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">12. Contact Us</h2>
+    <p class="mt-6">If you have any questions about these Terms, please contact us at esteban@thecreativeone.io</p>
+  </div>
+</div>
+</template>
+
+<script setup>
+
+definePageMeta({
+  layout: 'main'
+})
+
+
+</script>
