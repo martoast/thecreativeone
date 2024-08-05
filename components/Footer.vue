06bdbaaa5f2a9b5b3330334b889df5658c9c6a59
martoast <alexmartos96@gmail.com>
2024-08-05 16:24:35 -0700

    add land to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -29,6 +29,7 @@ const navigation = {
     { name: 'Get on my investors list', href: '/get-on-my-investors-list' },
     { name: 'Creative REI', href: 'https://upgrade.chat/creative-rei' },
     { name: 'Privacy policy', href: '/privacy-policy' },
+    { name: 'Land', href: '/land' },
 
     
   ],
