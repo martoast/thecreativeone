7310a9385d6f542160e7a2a1e21a72faccb51d0f
martoast <alexmartos96@gmail.com>
2024-09-03 18:57:36 -0700

    fix

--- pages/jv.vue
+++ pages/jv.vue
@@ -217,8 +217,8 @@
           <label for="skool" class="block text-sm font-semibold leading-6 text-gray-900">Are you a Skool member?</label>
           <select id="skool" name="skool" v-model="form.skool" class="mt-2.5 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
             <option value="">Please Select</option>
-            <option value="good">Yes</option>
-            <option value="fair">No</option>
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
           </select>
         </div>
         </div>
