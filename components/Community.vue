590590e0a9e421bde4020e1bbf8336e618776b51
martoast <alexmartos96@gmail.com>
2024-10-09 10:48:54 -0700

    instragram feed into site

--- components/Community.vue
+++ components/Community.vue
@@ -2,7 +2,7 @@
     <div class="mt-24 mb-6 overflow-hidden sm:mt-32">
         <div class="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
           <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
-            <div class="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
+            <div class="lg:col-end-1 lg:w-full lg:max-w-2xl lg:pb-8">
               <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Teaching others how to invest in Real Estate</h2>
               <p class="mt-6 text-xl leading-8 text-gray-600">Hey, my name is Esteban. Like most people, I wasn’t born into wealth or this profession. I started in the medical field at the lower end of the pay scale, initially making $12.40 an hour. Over time, I built a reputation for myself in the medical field, taking on numerous travel contracts with federal and state entities.</p>
               <p class="mt-6 text-base leading-7 text-gray-600">Eventually, I found a way to bridge the gap between my medical knowledge and real estate investing. When I first heard about wholesaling, I closed a handful of deals and then decided to focus on buying and building my rental portfolio to create passive income and buy back my time.</p>
@@ -11,9 +11,8 @@
             </div>
             <div class="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
               <div class="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
-                <img src="https://green-junior-chickadee-977.mypinata.cloud/ipfs/QmSabPGPR2dFEZBAw4sLAfKVUQJcCxAugjctWqECPkZcKo" alt="" class="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover" />
+                <iframe height="585px" src="https://www.instagram.com/p/C93DN0qxASh/embed/" frameborder="0"></iframe>
               </div>
-              
             </div>
           </div>
         </div>
