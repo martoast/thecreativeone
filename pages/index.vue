968a940855349812a10eddb81c8a277e86931024
martoast <alexmartos96@gmail.com>
2024-10-09 17:14:14 -0700

    remove section

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,42 @@
 
     <Community/>
 
-    <MyNetwork/>
+    <div class="bg-white py-8 sm:py-24 my-12">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:mx-0">
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Let's work together</h2>
+            <p class="mt-6 text-lg leading-8 text-gray-600">Whether you're selling a property, looking for the next investment opportunity, or need funding for your real estate projects, we've got you covered. Explore our services to get started on your next venture with ease and confidence.</p>
+        </div>
+          
+        <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+          <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+            <div v-for="feature in features" :key="feature.name" class="flex flex-col">
+              <dt class="text-base font-semibold leading-7 text-gray-900">
+                <div class="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                  <component :is="feature.icon" class="h-6 w-6 text-white" aria-hidden="true" />
+                </div>
+                {{ feature.name }}
+              </dt>
+              <dd class="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                <p class="flex-auto">{{ feature.description }}</p>
+                <p class="mt-6">
+                  <a :href="feature.href" class="text-sm font-semibold leading-6 text-indigo-600">Learn more <span aria-hidden="true">→</span></a>
+                </p>
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </div>
+
+
+    <Testimonial/>
 
     
 
-    <Testimonial/>
+    
+
+    
     <!-- <LogoCloud/> -->
     <Features/>
 
@@ -56,33 +87,7 @@
 
     
     
-    <div class="bg-white py-18 sm:py-24 my-12">
-      <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <div class="mx-auto max-w-2xl lg:mx-0">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Let's work together</h2>
-            <p class="mt-6 text-lg leading-8 text-gray-600">Whether you're selling a property, looking for the next investment opportunity, or need funding for your real estate projects, we've got you covered. Explore our services to get started on your next venture with ease and confidence.</p>
-        </div>
-          
-        <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
-          <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
-            <div v-for="feature in features" :key="feature.name" class="flex flex-col">
-              <dt class="text-base font-semibold leading-7 text-gray-900">
-                <div class="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                  <component :is="feature.icon" class="h-6 w-6 text-white" aria-hidden="true" />
-                </div>
-                {{ feature.name }}
-              </dt>
-              <dd class="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600">
-                <p class="flex-auto">{{ feature.description }}</p>
-                <p class="mt-6">
-                  <a :href="feature.href" class="text-sm font-semibold leading-6 text-indigo-600">Learn more <span aria-hidden="true">→</span></a>
-                </p>
-              </dd>
-            </div>
-          </dl>
-        </div>
-      </div>
-    </div>
+    
 
     <!-- <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
     <div class="mx-auto max-w-2xl sm:text-center">
