b2d95ae5e9981797e22b7c30af28eeedba1326ab
martoast <alexmartos96@gmail.com>
2025-02-10 13:17:57 -0800

    fix website instafeed and direct to propquest

--- layouts/main.vue
+++ layouts/main.vue
@@ -65,7 +65,7 @@
             <div class="flex-shrink-0">
               <button @click="handleNavigation" type="button" class="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 <MagnifyingGlassIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
-                Search property
+                Checkout PropQuest.ai
               </button>
             </div>
           </div>
@@ -111,7 +111,7 @@ const isActiveRoute = (route) => {
   return currentPath.startsWith(route) && (currentPath === route || currentPath.charAt(route.length) === '/');
 };
 
-const handleNavigation = async () => {
-  await navigateTo('/realty-radar')
+const handleNavigation = () => {
+  window.location.href = 'https://propquest.ai/'
 }
 </script>
